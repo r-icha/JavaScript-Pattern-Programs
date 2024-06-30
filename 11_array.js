2f22057f2238a8072a937c711384ecd8b4be4240
Richa Chaudhary <richachaudhary.csjm@gmail.com>
2024-06-30 14:17:41 +0530

    array that prints only two values from the length 10 array that values are not repeating

---
+++ 11_array.js
@@ -0,0 +1,22 @@
+const prompt = require("prompt-sync")();
+console.log("Enter the value of an array:");
+const arr = [];
+
+
+for (let i = 0; i <10; i++) {
+    const val = prompt();
+    arr[i] = val;
+}
+
+for (let i = 0; i < arr.length; i++) {
+    const temp = arr.filter((elem) => {
+        if (i == elem) {
+            return i;
+        }
+    })
+
+    if (temp.length == 1) {
+        console.log("Final value is:");
+        console.log("The value : "+ temp[0] + " ");
+    }
+}
