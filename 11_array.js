0927b45d5ea5c443c302360e9866011723472bfd
Richa Chaudhary <richachaudhary.csjm@gmail.com>
2024-07-01 17:21:25 +0530

    useing object

--- 11_array.js
+++ 11_array.js
@@ -1,26 +1,4 @@
  const prompt = require("prompt-sync")();
-// console.log("Enter the value of an array:");
-// const arr = [];
-
-
-// for (let i = 0; i <10; i++) {
-//     const val = prompt();
-//     arr[i] = val;
-// }
-
-// for (let i = 0; i < arr.length; i++) {
-//     const temp = arr.filter((elem) => {
-//         // if (i == elem) {
-//             return i==elem;
-//         // }
-//     })
-
-//     if (temp.length == 1) {
-//         console.log("Final value is:");
-//         console.log("The value : "+ temp[0] + " ");
-//     }
-// }
-
 
 const arr = [];
 const counts = {};
@@ -43,9 +21,48 @@ for (let i = 0; i < arr.length; i++) {
 }
 
 // Print non-repeating values
+console.log("the remainig elemnts is:")
 for (let i = 0; i < arr.length; i++) {
     const val = arr[i];
     if (counts[val] === 1) {
         console.log(val);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Enter the value of an array:");
+// const arr = [];
+
+
+// for (let i = 0; i <10; i++) {
+//     const val = prompt();
+//     arr[i] = val;
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+//     const temp = arr.filter((elem) => {
+//         // if (i == elem) {
+//             return i==elem;
+//         // }
+//     })
+
+//     if (temp.length == 1) {
+//         console.log("Final value is:");
+//         console.log("The value : "+ temp[0] + " ");
+//     }
+// }
